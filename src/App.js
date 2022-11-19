d95d14b00bd7d1fd6f97486573612e98fe346e6c
fakhrurrizal <fakhrurrizal10@gmail.com>
2022-11-19 22:04:55 +0700

    Add Product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import './App.css'
 // PAGES
 import Navbars from "./component/Navbars";
 import HomePage from "./page/Home";
+import Profile from "./page/user/Profile";
 import AdminPage from "./page/AdminPage";
 import AdminControlPage from "./page/admin/AdminControlPage";
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/admin/add-product" element={<AdminControlPage />} />
         <Route path="/admin/add-topping" element={<AdminControlPage />} />
       </Routes>
--- src/component/listProduk.jsx
+++ src/component/listProduk.jsx
@@ -1,53 +1,54 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card'
-import Stack from 'react-bootstrap/Stack';
-import product1 from '../assets/image/Produk1.png'
-import product2 from '../assets/image/Produk2.png'
-import product3 from '../assets/image/Produk3.png'
-import product4 from '../assets/image/Produk4.png'
+import { Stack, Card, Col } from 'react-bootstrap'
+// import product1 from '../assets/image/Produk1.png'
+// import product2 from '../assets/image/Produk2.png'
+// import product3 from '../assets/image/Produk3.png'
+// import product4 from '../assets/image/Produk4.png'
 
 export default function Produk(){
+
+    const Products = [];
+    const getProducts = () => {
+        if (typeof Storage === "undefined"){
+            alert("Tidak dapat menambahkan Product")
+        }
+
+        const localData = localStorage.getItem("DATA_PRODUCT");
+        let data = JSON.parse(localData);
+
+        let formater = new Intl.NumberFormat(undefined, {
+            style: "currency",
+            currency: "IDR",
+            maximumFractionDigits: 0,
+        });
+
+        if(data !== null) {
+            for (let i = 0; i < data.length; i++){
+                data[i].itemprice = formater.format(data[i].itemprice)
+                Products.push(data[i]);
+            }
+        }
+    };
+    getProducts();
+
     return(
         <>  
             <p className='fw-bold' style={{color:"#BD0707", fontSize:"36px", marginLeft:"10%", marginTop:"43%"}}>Let's Order</p>    
              <Stack direction="horizontal"  style={{marginLeft:"10%", height:"100vh", marginTop:'-110px',}}>
-                <Card className="" style={{width:'17rem',borderRadius:'14px',marginRight:'45px',  backgroundColor:"#F3CFC6"}}>
-                <Card.Img variant="top" src={ product1 } />
-                <Card.Body>
-                    <Card.Title>Ice Coffee Palm Sugar</Card.Title>
-                    <Card.Text>
-                    Rp. 27.000
-                    </Card.Text>
-                </Card.Body>
-                </Card>
-                <Card className="" style={{width:'17rem',borderRadius:'14px',marginRight:'45px',  backgroundColor:"#F3CFC6"}}>
-                <Card.Img variant="top" src={ product2 } />
-                <Card.Body>
-                    <Card.Title>Ice Coffee Green Tea</Card.Title>
-                    <Card.Text>
-                    Rp. 31.000
-                    </Card.Text>
-                </Card.Body>
-                </Card>
-                <Card className="" style={{width:'17rem',borderRadius:'14px',marginRight:'45px',  backgroundColor:"#F3CFC6"}}>
-                <Card.Img variant="top" src={ product3 } />
-                <Card.Body>
-                    <Card.Title>Hanami Latte</Card.Title>
-                    <Card.Text>
-                    Rp. 29.000
-                    </Card.Text>
-                </Card.Body>
-                </Card>
-                <Card className="" style={{width:'17rem',borderRadius:'14px',marginRight:'45px',  backgroundColor:"#F3CFC6"}}>
-                <Card.Img variant="top" src={ product4 } />
-                <Card.Body>
-                    <Card.Title>Clepon Coffee</Card.Title>
-                    <Card.Text>
-                    Rp. 28.000
-                    </Card.Text>
-                </Card.Body>
-                </Card>
+                {Products.map((item, index) => (
+                    <Col>
+                    <Card className="" style={{width:'17rem',borderRadius:'14px',marginRight:'45px',  backgroundColor:"#F3CFC6"}}>
+                        <Card.Img variant="top" src={item.itemimage}  />
+                        <Card.Body>
+                            <Card.Title>{item.itemname}</Card.Title>
+                            <Card.Text>
+                            {item.itemprice}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                ))}
              </Stack>
         </>
     )
